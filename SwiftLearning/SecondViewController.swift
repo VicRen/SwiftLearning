3ce062d1b57ae236d03c7b0b5278e18cf31cfba3
Vic Ren <vic.ren@juphoon.com>
2015-05-23 19:14:41 +0800

    Create Swift Learning Program
    I’m learning swift now. :）

---
+++ SwiftLearning/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwiftLearning
+//
+//  Created by Vic Ren on 15/5/23.
+//  Copyright (c) 2015年 Vic Ren. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
