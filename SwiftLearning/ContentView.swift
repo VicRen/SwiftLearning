2b36fd76f961aebbaa49cbb91911bd3013e3761f
VicRen <vic.ren@me.com>
2020-05-29 20:40:56 +0800

    CS193p 79:00

--- SwiftLearning/ContentView.swift
+++ SwiftLearning/ContentView.swift
@@ -28,16 +28,18 @@ struct CardView:View {
     var card: MemoryGame<String>.Card
     
     var body: some View{
-        ZStack {
-            if card.isFaceUP {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text(card.content)
-            } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
+        GeometryReader(content: { geometry in
+            ZStack {
+                if self.card.isFaceUP {
+                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    Text(self.card.content)
+                } else {
+                    RoundedRectangle(cornerRadius: 10.0).fill()
 
+                }
             }
-        }
+        })
     }
 }
 
