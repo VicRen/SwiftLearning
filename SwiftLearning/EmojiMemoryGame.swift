28d4259b26af5c5734b5efd6bb9e450655c7742e
Vic Ren <vic.ren@me.com>
2025-01-15 19:25:24 +0800

    add EmojiMemoryGame logic

--- SwiftLearning/EmojiMemoryGame.swift
+++ SwiftLearning/EmojiMemoryGame.swift
@@ -8,5 +8,29 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
+    private static let emojis: Array<String> = ["👻", "🎃", "🦇","🧛","⚰️","🪄","🔮","🧿","🦄","🍭","🧙","🧌"]
     
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame<String>(numberOfPairesOfCards: 8) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                emojis[pairIndex]
+            } else {
+                "⁉️"
+            }
+        }
+    }
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
 }
